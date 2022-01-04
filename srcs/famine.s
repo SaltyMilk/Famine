e36b2cbf184d752f5abda1dcbf0c6ed43b5c5354
SaltyMilk <soufiane.elmelcaoui@gmail.com>
2022-01-05 00:56:48 +0100

    directory listing

--- srcs/famine.s
+++ srcs/famine.s
@@ -1,1 +1,106 @@
 ;let's do this
+
+global _start
+
+_start:
+	push 0x0000002e; our target directory here "."
+	lea rdi, [rsp]
+	call list_files
+	sub rsp, 8
+
+exit_prog:
+	mov	rax, 0x3c;
+	mov rdi, 1
+	syscall
+
+;this will simply print a newline
+debug:
+	push rbx
+	push rcx
+	push r8
+	push r9
+	push r10
+	push rax
+	push rsi
+	push rdx
+	push rdi
+
+	mov rax, 1
+	push 0x0000000a
+	lea rsi, [rsp]
+	mov rdx, 2
+	mov rdi, 1
+	syscall
+	add rsp, 8
+	pop rdi
+	pop rdx
+	pop rsi
+	pop rax
+	pop r10
+	pop r9
+	pop r8
+	pop rcx
+	pop rbx
+retn
+
+ft_strlen:
+	xor rax, rax
+	loop:
+		cmp byte[rdi + rax], 0
+		je strlen_exit
+		inc rax
+		jmp loop
+	strlen_exit:
+retn
+
+list_files:
+	sub rsp, 1024;this is gonna be our buffer
+	sub rsp, 4; fd
+	mov rsi, 65536; O_RDONLY | O_DIRECTORY
+	xor rdx, rdx
+	mov rax, 2; sycall open
+	syscall
+	cmp rax, -1
+	je exit_prog ;open error
+ 	mov [rsp], rax
+	dir_read_loop:
+		mov rdi, [rsp];fd of dir
+		lea rsi, [rsp + 4]; addr of buffer
+		mov rdx, 1024;reading max 1024 bytes
+		mov rax, 217;getdents64 syscall
+		syscall
+		cmp rax, -1
+		je exit_prog;getdents64 err
+		cmp rax, 0
+		je dir_read_exit; done reading dir
+		mov r10, rax; store number bytes read
+		xor rcx, rcx; will serve as index to parse files
+		parse_file_loop:
+			mov r8, rsi; save rsi
+			lea r9, [rsi + rcx] ; current linux_dirent64*
+			lea rsi, [r9 + 19];filename
+			
+			mov rdi, rsi; get size of filename
+			call ft_strlen
+			mov rdx, rax;write filename size
+		
+			mov rdi, 1;stdout
+			mov rax, 1; write
+			push rcx; save rcx syscall will change it
+			syscall
+			pop rcx
+			call debug
+			mov rsi, r8
+			xor rdx, rdx
+			mov dx, WORD[r9 + 16]
+			add rcx, rdx
+			cmp rcx, r10
+			jae parse_file_exit
+			jmp parse_file_loop
+		parse_file_exit: 
+		jmp dir_read_loop
+	dir_read_exit:
+	add rsp, 4
+	add rsp,1024
+retn
+
